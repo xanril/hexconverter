c0e42558eda2948056b1e8d79e3e0f7132cdf747
Kristian Mitra <motionsmith.ph@gmail.com>
2018-01-30 08:43:32 +0800

    Added custom text field for validation implementation

---
+++ iOS/HexaTextField.cs
@@ -0,0 +1,19 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace HexConverter.iOS
+{
+    public partial class HexaTextField : UITextField
+    {
+        public HexaTextField (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override bool ShouldChangeTextInRange(UITextRange inRange, string replacementText)
+        {
+            Console.WriteLine("Replacement String: " + replacementText);
+            return base.ShouldChangeTextInRange(inRange, replacementText);
+        }
+    }
+}
