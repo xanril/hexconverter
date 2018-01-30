d9ab4055ac1664b2f96ec693f19c7fc9f293b971
Kristian Mitra <xanril@users.noreply.github.com>
2018-01-30 09:41:41 +0800

    Code cleanup on iOS implementation

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -6,8 +6,6 @@ namespace HexConverter.iOS
 {
     public partial class ViewController : UIViewController
     {
-        int count = 1;
-
         private HexadecimalConverter hexaConverter;
 
         public ViewController(IntPtr handle) : base(handle)
@@ -46,17 +44,22 @@ namespace HexConverter.iOS
             ConvertButton.TouchUpInside += (object sender, EventArgs e) => 
             {
                 this.HexTextField.ResignFirstResponder();
-                this.hexaConverter.AssignHexValue(this.HexTextField.Text);
-
-                RedLabel.Text = this.hexaConverter.RedDecimal + "";
-                GreenLabel.Text = this.hexaConverter.GreenDecimal + "";
-                BlueLabel.Text = this.hexaConverter.BlueDecimal + "";
-
-                ConvertedColorView.BackgroundColor = new UIColor(hexaConverter.RedDecimal / 255.0f,
-                                                                 hexaConverter.GreenDecimal / 255.0f,
-                                                                 hexaConverter.BlueDecimal / 255.0f,
-                                                                 1.0f);
+                Convert();
             };
+        }
+
+        private void Convert()
+        {
+            hexaConverter.AssignHexValue(this.HexTextField.Text);
+
+            RedLabel.Text   = hexaConverter.RedDecimal + "";
+            GreenLabel.Text = hexaConverter.GreenDecimal + "";
+            BlueLabel.Text  = hexaConverter.BlueDecimal + "";
+
+            ConvertedColorView.BackgroundColor = new UIColor(hexaConverter.RedDecimal / 255.0f,
+                                                             hexaConverter.GreenDecimal / 255.0f,
+                                                             hexaConverter.BlueDecimal / 255.0f,
+                                                             1.0f);
         }
     }
 }
